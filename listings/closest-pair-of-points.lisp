f9e967c2d7c71df6607ba8fca5f8f8a437228fbb
Massimo Nocentini <massimo.nocentini@gmail.com>
2011-10-16 18:10:53 +0200

    Tested some SLIME functionality for editing .lisp sources.

--- listings/closest-pair-of-points.lisp
+++ listings/closest-pair-of-points.lisp
@@ -1,14 +1,21 @@
-(defun make-set-of-point-definition (lst-of-pairs)
-  (mapcar (function (lambda (pair) (list :x (car pair) :y (car (cdr pair))))) lst-of-pairs))
-
 (defun introduce-sorting-info (lst-of-point-definitions)
-  (mapcar (function (lambda (point-definition) (append point-definition (list :x-position -1 :y-position -1)))) lst-of-point-definitions))
+  (mapcar (function 
+	   (lambda (point-definition) 
+	    (append point-definition 
+		    (list :x-position -1 :y-position -1)))) 
+	  lst-of-point-definitions))
 
 (defun sort-by-ascissa (lst)
   (sort (copy-list lst) (function <) :key 
 	(function 
 	 (lambda (lst-of-point-info) 
 	  (getf lst-of-point-info :x)))))
+
+(defun make-set-of-point-definition (lst-of-pairs)
+  (mapcar (function
+	   (lambda (pair) 
+	    (list :x (car pair) :y (car (cdr pair))))) 
+	  lst-of-pairs))
 
 (defun assign-ordinate-position (lst pos)
   (if (null lst)
@@ -24,7 +31,7 @@
 	  (getf lst-of-point-info :y)))))
 
 (defun assign-ascissa-position (lst pos)
-  (if (null lst)
+  (if (null lst) 
       ()
       (progn
 	(setf (getf (car lst) :x-position) pos)
@@ -32,4 +39,17 @@
 
 
 (defun dummy-test ()
-  (sort-by-ascissa (introduce-sorting-info (make-set-of-point-definition (list '(5 2) '(1 10) '(3 4))))))
+  (sort-by-ascissa 
+   (introduce-sorting-info 
+    (make-set-of-point-definition (list '(5 2) '(1 10) '(3 4))))))
+
+(defun e ()
+  "A badly indented function."
+  (let ((x 20))
+    (loop for i from 0 to x 
+       do (loop for j from 0 below 10 
+	     do (print j)) 
+       (if (< i 10)
+	   (let ((z nil) )
+	     (setq z (format t "x=~d" i))
+	     (print z))))))
