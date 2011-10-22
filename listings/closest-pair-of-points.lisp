97b309b3ad9ac865bf69f916a771bad611923f9a
Massimo Nocentini <massimo.nocentini@gmail.com>
2011-10-22 12:42:05 +0200

    Moved all test code in tail of the source file. Added some comments.

--- listings/closest-pair-of-points.lisp
+++ listings/closest-pair-of-points.lisp
@@ -5,9 +5,6 @@
 ;; in order to run the tests perform the following action
 ;; 1- open the file "lisp-unit.lisp" compile and load it with C-c C-k
 ;; 2- open this file and load and compile it too
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  ;(require :lisp-unit "lisp-unit.lisp")
-  (use-package :lisp-unit))
 
 (defun introduce-sorting-info (lst-of-point-definitions)
 "This function add the sorting information to be used during the algorithm."
@@ -20,19 +17,14 @@
 		    (list :x-position -1 :y-position -1)))) 
 	  lst-of-point-definitions))
 
-(lisp-unit:define-test introduce-sorting-info-test 
-  (assert-equal () (introduce-sorting-info ()))
-  (assert-equal (list 
-		 '(:X 5 :Y 2 :X-POSITION -1 :Y-POSITION -1) 
-		 '(:X 1 :Y 10 :X-POSITION -1 :Y-POSITION -1) 
-		 '(:X 3 :Y 4 :X-POSITION -1 :Y-POSITION -1)) 
-		(introduce-sorting-info 
-		 (list 
-		  '(:X 5 :Y 2) 
-		  '(:X 1 :Y 10) 
-		  '(:X 3 :Y 4)))))
-
-;;--------------------------------------------------------------------------
+(defun make-set-of-point-definition (lst-of-pairs)
+  "This function build a plist with only information on ascissa and
+ordinata"
+  (mapcar (function
+	   (lambda (pair) 		; for each pair we return a
+					; list more structured
+	    (list :x (car pair) :y (car (cdr pair))))) 
+	  lst-of-pairs))
 
 (defun sort-by-ascissa (lst)
   "this method return a new list of plists ordered by the :x component"
@@ -60,6 +52,60 @@ ascending order, using the function object key-retriever-function to find
 the object to be used from each element to compare for the ordering."
   (sort (copy-list lst) (function <) 
 	:key key-retriever-function))
+
+(defun assign-ordinate-position (lst)
+  (assign-positions-on (function y-position-component-getter) lst 0))
+
+(defun assign-ascissa-position (lst)
+  (assign-positions-on (function x-position-component-getter) lst 0))
+
+(defun assign-positions-on (position-component-getter-function lst pos)
+  "This is the motor that recursively scan the lst, visiting each
+element in the given order, and assign its position by attach an integer."
+  (if (null lst) 
+      ;;  base case: we have processed all items, assigning to each one
+      ;; its position by a bijection on integers
+      lst			     
+      (progn
+	(funcall position-component-getter-function (car lst) pos)
+	(assign-positions-on 
+	 position-component-getter-function (cdr lst) (+ pos 1)))))  
+
+(defun x-position-component-getter (plist pos)
+  "this method, given a plist representing a point and the position, 
+set the :x-position component to position"
+   (setf (getf plist :x-position) pos))
+
+(defun y-position-component-getter (plist pos)
+ "this method, given a plist representing a point and the position, 
+set the :y-position component to position"
+   (setf (getf plist :y-position) pos))
+			    
+;; ---------------------UNIT TESTS----------------------
+
+(lisp-unit:define-test introduce-sorting-info-test 
+  ;; this assertion check that on empty list nothing will be added
+  (assert-equal () (introduce-sorting-info ()))
+  ;; this assertion assure that the information on x and y ordering
+  ;; are introduced (but for now initialized to -1 both)
+  (assert-equal (list 
+		 '(:X 5 :Y 2 :X-POSITION -1 :Y-POSITION -1) 
+		 '(:X 1 :Y 10 :X-POSITION -1 :Y-POSITION -1) 
+		 '(:X 3 :Y 4 :X-POSITION -1 :Y-POSITION -1)) 
+		(introduce-sorting-info 
+		 (list 
+		  '(:X 5 :Y 2) 
+		  '(:X 1 :Y 10) 
+		  '(:X 3 :Y 4)))))
+
+(lisp-unit:define-test make-set-of-point-definition-test 
+  (assert-equal () (make-set-of-point-definition ()))
+  (assert-equal (list 
+		 '(:X 5 :Y 2) 
+		 '(:X 1 :Y 10) 
+		 '(:X 3 :Y 4)) 
+		(make-set-of-point-definition 
+		 (list '(5 2) '(1 10) '(3 4)))))
 
 (lisp-unit:define-test sort-by-ascissa-test 
   (assert-equal () (sort-by-ascissa ()))
@@ -104,54 +150,3 @@ the object to be used from each element to compare for the ordering."
 		   '(:X 5 :Y 2 :X-POSITION -1 :Y-POSITION -1) 
 		   '(:X 1 :Y 10 :X-POSITION -1 :Y-POSITION -1) 
 		   '(:X 3 :Y 4 :X-POSITION -1 :Y-POSITION -1))))))
-
-
-
-;; ------------------------------------------------------
-(defun make-set-of-point-definition (lst-of-pairs)
-  "This function build a plist with only information on ascissa and
-ordinata"
-  (mapcar (function
-	   (lambda (pair) 		; for each pair we return a
-					; list more structured
-	    (list :x (car pair) :y (car (cdr pair))))) 
-	  lst-of-pairs))
-
-
-(lisp-unit:define-test make-set-of-point-definition-test 
-  (assert-equal () (make-set-of-point-definition ()))
-  (assert-equal (list 
-		 '(:X 5 :Y 2) 
-		 '(:X 1 :Y 10) 
-		 '(:X 3 :Y 4)) 
-		(make-set-of-point-definition 
-		 (list '(5 2) '(1 10) '(3 4)))))
-;; ------------------------------------------------------
-
-(defun assign-ordinate-position (lst)
-  (assign-positions-on (function y-position-component-getter) lst 0))
-
-(defun assign-ascissa-position (lst)
-  (assign-positions-on (function x-position-component-getter) lst 0))
-
-(defun assign-positions-on (position-component-getter-function lst pos)
-  "This is the motor that recursively scan the lst, visiting each
-element in the given order, and assign its position by attach an integer."
-  (if (null lst) 
-      ;;  base case: we have processed all items, assigning to each one
-      ;; its position by a bijection on integers
-      lst			     
-      (progn
-	(funcall position-component-getter-function (car lst) pos)
-	(assign-positions-on 
-	 position-component-getter-function (cdr lst) (+ pos 1)))))  
-
-(defun x-position-component-getter (plist)
-  "this method, given a plist, return its :x-position component."
-   (getf plist :x-position))
-
-(defun y-position-component-getter (plist)
-  "this method, given a plist, return its :y-position component."
-   (getf plist :y-position))
-			    
-;; ------------------------------------------------------
